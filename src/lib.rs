6e58312ef183cac4f14a4b4a5ee075b5ec14bfcb
Wei Tang <hi@that.world>
2017-06-30 22:12:38 +0800

    Implement make_cache

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 // The reference algorithm used is from https://github.com/ethereum/wiki/wiki/Ethash
 
+extern crate sha3;
+
 mod miller_rabin;
 use miller_rabin::is_prime;
+use sha3::{Digest, Keccak256, Keccak512};
+use std::ops::BitXor;
 
 const WORD_BYTES: usize = 4;
 const DATASET_BYTES_INIT: usize = 1073741824; // 2 to the power of 30.
@@ -32,6 +36,44 @@ fn get_full_size(block_number: usize) -> usize {
         sz -= 2 * MIX_BYTES
     }
     sz
+}
+
+fn fill_sha512(input: &[u8], a: &mut [u8], from_index: usize) {
+    let mut hasher = Keccak512::default();
+    hasher.input(input);
+    let out = hasher.result();
+    for i in 0..out.len() {
+        a[from_index + i] = out[i];
+    }
+}
+
+fn fill_sha256(input: &[u8], a: &mut [u8], from_index: usize) {
+    let mut hasher = Keccak256::default();
+    hasher.input(input);
+    let out = hasher.result();
+    for i in 0..out.len() {
+        a[from_index + i] = out[i];
+    }
+}
+
+pub fn make_cache(cache: &mut [u8], seed: [u8; 64]) {
+    let n = cache.len() / HASH_BYTES;
+    fill_sha512(&seed, cache, 0);
+    for i in 1..n {
+        let (last, next) = cache.split_at_mut(i * 64);
+        fill_sha512(last, next, 0);
+    }
+    for _ in 0..CACHE_ROUNDS {
+        for i in 0..n {
+            let v = (cache[i * 64] as usize) % n;
+            let mut r = [0u8; 64];
+            for j in 0..64 {
+                let a = cache[((i - 1 + n) % n) * 64 + j];
+                let b = cache[v * 64 + j];
+                r[j] = a.bitxor(b);
+            }
+        }
+    }
 }
 
 #[cfg(test)]
